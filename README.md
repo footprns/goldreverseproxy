7719dff22b5796e88a1e38d70e3a93eac43d35f0
footprns <imankurniawan@gmail.com>
2017-01-18 18:53:48 +0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# goldreverseproxy
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: goldreverseproxy
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
